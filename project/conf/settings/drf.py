3b879c33fad7a2cc03b708d69c7f7241ea65797a
Cat Developer <catdeveloper-com@yandex.ru>
2025-01-28 21:38:54 +0300

    Произведена базовая настройка DRF

--- project/conf/settings/drf.py
+++ project/conf/settings/drf.py
@@ -1,1 +1,11 @@
 """Настройки DRF."""
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.XMLRenderer',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
