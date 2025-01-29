ff1f052ba1e070d9abf2cb7d3959a21561169eba
Cat Developer <catdeveloper-com@yandex.ru>
2025-01-29 04:08:32 +0300

    Исправлены названия ENV переменных

--- project/conf/settings/database.py
+++ project/conf/settings/database.py
@@ -5,11 +5,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DJ_DB_NAME'),
-        'USER': os.environ.get('DJ_DB_USER'),
-        'PASSWORD': os.environ.get('DJ_DB_PASSWORD'),
-        'HOST': os.environ.get('DJ_DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DJ_DB_PORT', 5432),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
         'OPTIONS': {
             # Нужно явно указать схемы, с которыми будет работать приложение.
             'options': '-c search_path=public'
